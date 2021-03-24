52fcf2e614b2970bdd76b1c4b7497746f5384cec
Abdullah SISSOKO <abdullah.sissoko@ens.uvsq.fr>
2021-03-24 18:49:23 +0100

    test

---
+++ entrainement/percGC.py
@@ -0,0 +1,5 @@
+percGC = ((4500 + 2575)/14800)*100
+print("Le pourcentage de GC est {:.0f} %".format(percGC))
+print("Le pourcentage de GC est {:.1f} %".format(percGC))
+print("Le pourcentage de GC est {:.2f} %".format(percGC))
+print("Le pourcentage de GC est {:.3f} %".format(percGC))
